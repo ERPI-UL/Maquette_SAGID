49dfb6c58812a622f91f29dbad73dcf8196c7984
Sébastien Demange <s.demange@actibac.fr>
2023-09-30 01:30:36 +0200

    [Refactoring] simplify SliderTextFieldForm code

--- src/form/SliderTextFieldForm.jsx
+++ src/form/SliderTextFieldForm.jsx
@@ -40,169 +40,58 @@ const StyledFormControl = styled(FormControl)({
 
 const SliderTextFieldForm = ({ id, onChange, currentData }) => {
 
-    let initialTextFieldValue = "0";
+  const parametres = currentData.planEntretien[4].parametres;
 
-    const [sliderValues, setSliderValues] = useState({
-        slider1: currentData.planEntretien[4].parametres[0].value,
-        slider2: currentData.planEntretien[4].parametres[1].value,
-        slider3: currentData.planEntretien[4].parametres[2].value,
-        slider4: currentData.planEntretien[4].parametres[5].value,
-        slider5: currentData.planEntretien[4].parametres[6].value,
-    });
-    
-    if (id === "slider1") {
-        initialTextFieldValue = currentData.planEntretien[4].parametres[0].value;
-    } else if (id === "slider2") {
-        initialTextFieldValue = currentData.planEntretien[4].parametres[1].value;
-    } else if (id === "slider3") {
-        initialTextFieldValue = currentData.planEntretien[4].parametres[2].value;
-    } else if (id === "slider4") {
-        initialTextFieldValue = currentData.planEntretien[4].parametres[5].value;
-    } else if (id === "slider5") {
-        initialTextFieldValue = currentData.planEntretien[4].parametres[6].value;
-    }
+  const [values, setValues] = useState({
+      slider1: parametres[0].value,
+      slider2: parametres[1].value,
+      slider3: parametres[2].value,
+      slider4: parametres[5].value,
+      slider5: parametres[6].value,
+  });
 
-    const [textFieldValue, setTextFieldValue] = useState(initialTextFieldValue);
-    const maxValue = 100;
-    const minValue = 0;
-    const stepValue = 1;
+  const maxValue = 100;
+  const minValue = 0;
+  const stepValue = 1;
 
-    const handleSliderChange = _.debounce((id, newValue) => {
-
-        // Copiez les valeurs actuelles
-        const updatedValues = { ...sliderValues };
-        updatedValues[id] = newValue;
-
-        // let slider1Value, slider2Value, slider3Value, slider4Value, slider5Value = 0;
-        // const newCurrentData = { ...currentData };
-
-        // currentData.planEntretien[4].parametres[0].value = updatedValues.slider1;
-        // currentData.planEntretien[4].parametres[1].value = updatedValues.slider2;
-        // currentData.planEntretien[4].parametres[2].value = updatedValues.slider3;
-        // currentData.planEntretien[4].parametres[5].value = updatedValues.slider4;
-        // currentData.planEntretien[4].parametres[6].value = updatedValues.slider5;
-        // console.log("   --> handleSliderChange newCurrentData",newCurrentData);
-        // console.log("   --> handleSliderChange currentData",currentData);
-
-        // if (id === "slider1") {
-
-        //     slider1Value = newValue;
-        //     slider2Value = updatedValues.slider2;
-        //     slider3Value = updatedValues.slider3;
-        //     console.log(slider1Value, slider2Value, slider3Value);
-        //     if (slider2Value > slider3Value) {
-        //         console.log("slider2>slider3");
-        //         slider2Value = 100 - slider1Value - slider3Value;
-        //     } else {
-        //         console.log("slider3>slider2");
-        //        slider3Value = 100 - slider1Value - slider2Value;
-        //     }
-        //     updatedValues.slider1 = slider1Value;
-        //     updatedValues.slider2 = slider2Value;
-        //     updatedValues.slider3 = slider3Value;
-            
-
-        //     newCurrentData.planEntretien[4].parametres[0].value = updatedValues.slider1;
-        //     newCurrentData.planEntretien[4].parametres[1].value = updatedValues.slider2;
-        //     newCurrentData.planEntretien[4].parametres[2].value = updatedValues.slider3;
-        //     newCurrentData.planEntretien[4].parametres[5].value = updatedValues.slider4;
-        //     newCurrentData.planEntretien[4].parametres[6].value = updatedValues.slider5;
-        // }  else if (id === "slider2") {
-
-        //     slider2Value = newValue;
-        //     slider1Value = updatedValues.slider1;
-        //     slider3Value = updatedValues.slider3;
-
-        //     if (slider1Value > slider3Value) {
-        //         slider1Value = 100 - slider2Value - slider3Value;
-        //     } else if (slider3Value > slider2Value - 1) {
-        //         slider3Value = 100 - slider1Value - slider2Value;
-        //     }
-        //     updatedValues.slider1 = slider1Value;
-        //     updatedValues.slider2 = slider2Value;
-        //     updatedValues.slider3 = slider3Value;
-        //     console.log(updatedValues.slider1, updatedValues.slider2, updatedValues.slider3);
-
-        //     newCurrentData.planEntretien[4].parametres[0].value = updatedValues.slider1;
-        //     newCurrentData.planEntretien[4].parametres[1].value = updatedValues.slider2;
-        //     newCurrentData.planEntretien[4].parametres[2].value = updatedValues.slider3;
-        //     newCurrentData.planEntretien[4].parametres[5].value = updatedValues.slider4;
-        //     newCurrentData.planEntretien[4].parametres[6].value = updatedValues.slider5;
-        // } else if (id === "slider3") {
-        //     console.log("PROBLEME");
-        //     slider3Value = newValue;
-        //     slider1Value = updatedValues.slider1;
-        //     slider2Value = updatedValues.slider2;
-
-        //     if (slider1Value > slider2Value) {
-        //         slider1Value = 100 - slider3Value - slider2Value;
-        //     } else if (slider2Value > slider1Value - 1) {
-        //         slider2Value = 100 - slider1Value - slider3Value;
-        //     }
-        //     updatedValues.slider1 = slider1Value;
-        //     updatedValues.slider2 = slider2Value;
-        //     updatedValues.slider3 = slider3Value;
-        //     console.log(updatedValues.slider1, updatedValues.slider2, updatedValues.slider3);
-
-        //     newCurrentData.planEntretien[4].parametres[0].value = updatedValues.slider1;
-        //     newCurrentData.planEntretien[4].parametres[1].value = updatedValues.slider2;
-        //     newCurrentData.planEntretien[4].parametres[2].value = updatedValues.slider3;
-        //     newCurrentData.planEntretien[4].parametres[5].value = updatedValues.slider4;
-        //     newCurrentData.planEntretien[4].parametres[6].value = updatedValues.slider5;
-        // } else if (id === "slider4") {
-        //   newCurrentData.planEntretien[4].parametres[5].value = newValue;
-        // } else if (id === "slider5") {
-        //   newCurrentData.planEntretien[4].parametres[6].value = newValue;
-        // }
-        
-        // Mise à jour des sliders
-        setSliderValues(updatedValues);
-        // Mise à jour du champ texte
-        setTextFieldValue(newValue);
-        // Appelez la fonction de rappel pour informer le composant parent des changements
-        onChange(id, newValue);
-        }, 10);
- 
-
-  const handleTextFieldChange = (event) => {
-    let parsedValue = parseInt(event.target.value);
-    parsedValue = parsedValue < minValue ? minValue : parsedValue;
-    parsedValue = parsedValue > maxValue ? maxValue : parsedValue;
-    setTextFieldValue(parsedValue);
-    let newValues = {...sliderValues};
-    newValues[id] = parsedValue;
-    if (!isNaN(parsedValue)) {
-      setSliderValues(newValues);
-      onChange(id, parsedValue);
-    }
-  };
+  const handleChange = _.debounce((id, newValue) => {
+      // Copiez les valeurs actuelles
+      newValue = newValue < minValue ? minValue : newValue;
+      newValue = newValue > maxValue ? maxValue : newValue;
+      const updatedValues = { ...values };
+      updatedValues[id] = newValue;
+      // Mise à jour des sliders
+      setValues(updatedValues);
+      // Appelez la fonction de rappel pour informer le composant parent des changements
+      onChange(id, newValue);
+  }, 10);
 
   return (
-      <div className="row">
-        <div className="col-3">
-          <StyledFormControl >
-            <StyledNumberInput
-              type="number"
-              value={textFieldValue}
-              onChange={handleTextFieldChange}
-              variant="outlined"
-            />
-          </StyledFormControl>
-        </div>
-        <div className="col-9">
-          <StyledFormControl>
-            <StyledSlider
-              value={sliderValues[id]}  
-              onChange={(event, newValue) => handleSliderChange(id, newValue)}  
-              min={minValue}
-              max={maxValue}
-              step={stepValue}
-            />
-          </StyledFormControl>
-        </div>
+    <div className="row">
+      <div className="col-3">
+        <StyledFormControl >
+          <StyledNumberInput
+            type="number"
+            value={values[id]}
+            onChange={(event) => handleChange(id, parseInt(event.target.value))}
+            variant="outlined"
+          />
+        </StyledFormControl>
       </div>
+      <div className="col-9">
+        <StyledFormControl>
+          <StyledSlider
+            value={values[id]}  
+            onChange={(event, newValue) => handleChange(id, newValue)}  
+            min={minValue}
+            max={maxValue}
+            step={stepValue}
+          />
+        </StyledFormControl>
+      </div>
+    </div>
   );
-  };
+};
 
 export default SliderTextFieldForm;
 
