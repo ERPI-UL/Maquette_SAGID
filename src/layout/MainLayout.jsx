b0c6be86633005ea6324a36aeec0b8ff8b23a04e
Sébastien Demange <s.demange@actibac.fr>
2023-10-26 17:46:58 +0200

    [Feat] Add an app bar

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,21 @@
 import React  from 'react';
 
+import { Box, AppBar, Toolbar, Typography } from '@mui/material';
 import './main-layout.scss';
 import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
   return (
     <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              SAGID
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
       <div className="main">
         <div className="main__content">
           <Outlet />
