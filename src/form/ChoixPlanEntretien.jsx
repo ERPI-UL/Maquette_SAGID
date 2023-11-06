18646a5df6d8b40dd82a814d304901221a5ebe31
BriceCorrigeux <106590102+BriceCorrigeux@users.noreply.github.com>
2023-11-06 10:35:39 +0100

    correction bug netlify

--- src/form/ChoixPlanEntretien.jsx
+++ src/form/ChoixPlanEntretien.jsx
@@ -9,7 +9,7 @@ import {
     CardMedia,
     CardContent,
     CardActions,
-    Paper
+    
 } from "@mui/material";
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
