2b5e4e50f37b4a998a2c3fe941437d9800166e40
Sébastien Demange <s.demange@actibac.fr>
2023-11-07 09:50:29 +0100

    [Feat] Add a text on the first page

--- src/form/ChoixTerritoire.jsx
+++ src/form/ChoixTerritoire.jsx
@@ -34,80 +34,93 @@ const ChoixTerritoire = ( {currentData, setCurrentData, redirect }) => {
   }
 
   return (
-    <Grid className='choixEntretienPage' container direction={"row"} justifyContent="center" alignItems="center" spacing={2} sx={{
+    <Grid className='choixEntretienPage' container direction={"row"} justifyContent="center" alignItems="center" sx={{
       backgroundImage: `url(${process.env.PUBLIC_URL + '/images/route_departementale.jpg'})`,
       backgroundSize: 'cover',
       backgroundPosition: 'center',
       height:'100vh'
     }}>
-      <Grid item container direction="row" justifyContent="center" alignItems="center">
-        <Paper elevation={4} sx={{ minWidth: 350, maxWidth: 500, width: '70%', padding: 5 }} square={false}>
-          <Box component="form" sx={{ width: '100%' }} onSubmit={handleSubmit}>
-            <Grid container direction={"column"} spacing={2}>
+      
+      <Paper elevation={4} sx={{ minWidth: 700, maxWidth: 1000, width: '70%', padding: 5 }} square={false}>
+        <Grid item container direction="row" alignItems="center" justifyContent="center" spacing={5}>
+          <Grid item sx={{ width: '50%' }}>
+            <Typography variant="h3">
+              SAGID
+            </Typography>
+            <Typography paragraph={true} align="justify">
+              Découvrez l’influence de l’entretien des bords de route sur votre territoire et comment valoriser le patrimoine naturel présent sur vos dépendances vertes.
+              L’application SAGID permet d’obtenir des évaluations au niveau économique, au niveau des émissions de gaz à effet de serre et au niveau des services écosystémiques fournis par les bords de route.
+            </Typography>
+          </Grid>
+          <Grid item sx={{ width: '50%' }}>
+            <Box component="form" onSubmit={handleSubmit}>
+              <Grid container direction={"column"} spacing={2}>
 
-              <Grid item>
-                <Typography variant="h6">
-                  Je choisis mon territoire
-                </Typography>
-              </Grid>
+                <Grid item>
+                  <Typography variant="h6">
+                    Je choisis mon territoire
+                  </Typography>
+                </Grid>
 
-              <Grid item>
-                <FormControl fullWidth size={"small"}>
-                  <InputLabel id="region-select-label">Région</InputLabel>
-                  <Select
-                    labelId="region-select-label"
-                    id="region-select"
-                    value={territoire.region}
-                    label="Région"
-                    onChange={handleRegionChange}
+                <Grid item>
+                  <FormControl fullWidth size={"small"}>
+                    <InputLabel id="region-select-label">Région</InputLabel>
+                    <Select
+                      labelId="region-select-label"
+                      id="region-select"
+                      value={territoire.region}
+                      label="Région"
+                      onChange={handleRegionChange}
+                    >
+                      {GeoData.regions.list.map((region) => {
+                        return <MenuItem key={region.REG} value={region}>{region.LIBELLE}</MenuItem>
+                      })}
+                    </Select>
+                  </FormControl>
+                </Grid>
+
+                <Grid item>
+                  <FormControl
+                    fullWidth
+                    disabled={territoire.region === ''}
+                    size={"small"}
+                    sx={territoire.region === '' ? {opacity: 0} : {opacity: 1}}
                   >
-                    {GeoData.regions.list.map((region) => {
-                      return <MenuItem key={region.REG} value={region}>{region.LIBELLE}</MenuItem>
-                    })}
-                  </Select>
-                </FormControl>
-              </Grid>
+                    <InputLabel id="dept-select-label">Département</InputLabel>
+                    <Select
+                      labelId="dept-select-label"
+                      id="dept-select"
+                      value={territoire.departement}
+                      label="Département"
+                      onChange={handleDepartementChange}
+                    >
+                      {filteredDepartements().map((departement) => {
+                        return <MenuItem key={departement.DEP} value={departement}>{departement.DEP} - {departement.LIBELLE}</MenuItem>
+                      })}
+                    </Select>
+                  </FormControl>
+                </Grid>
 
-              <Grid item>
-                <FormControl
-                  fullWidth
-                  disabled={territoire.region === ''}
-                  size={"small"}
-                  sx={territoire.region === '' ? {opacity: 0} : {opacity: 1}}
-                >
-                  <InputLabel id="dept-select-label">Département</InputLabel>
-                  <Select
-                    labelId="dept-select-label"
-                    id="dept-select"
-                    value={territoire.departement}
-                    label="Département"
-                    onChange={handleDepartementChange}
-                  >
-                    {filteredDepartements().map((departement) => {
-                      return <MenuItem key={departement.DEP} value={departement}>{departement.DEP} - {departement.LIBELLE}</MenuItem>
-                    })}
-                  </Select>
-                </FormControl>
-              </Grid>
+                <Grid item>
+                  <FormControl fullWidth  size={"small"}>
+                    <Button
+                      color="primary"
+                      type="submit"
+                      disabled={territoire.departement === ''}
+                      variant={territoire.departement === '' ? "outlined" : "contained"}
+                      sx={territoire.departement === '' ? {opacity: 0} : {opacity: 1}}
+                    >
+                      Sélectionner
+                    </Button>
+                  </FormControl>
+                </Grid>
 
-              <Grid item>
-                <FormControl fullWidth  size={"small"}>
-                  <Button
-                    color="primary"
-                    type="submit"
-                    disabled={territoire.departement === ''}
-                    variant={territoire.departement === '' ? "outlined" : "contained"}
-                    sx={territoire.departement === '' ? {opacity: 0} : {opacity: 1}}
-                  >
-                    Sélectionner
-                  </Button>
-                </FormControl>
               </Grid>
+            </Box>
+          </Grid>
+        </Grid>
+      </Paper>
 
-            </Grid>
-          </Box>
-        </Paper>
-      </Grid>
     </Grid>
   );
 }
